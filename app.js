d170a3c59e0cccc1faa4504ddd23c4e5c03bde52
Klace Koch <koch.klace@gmail.com>
2020-09-17 12:33:00 -0700

    constructors done

--- app.js
+++ app.js
@@ -9,12 +9,26 @@
 
 // form.addEventListener('submit', logSubmit);
 
-var users = []
+var users = [];
+var chores = [];
+var currentDate = new Date();
 
-function AddUser (name){
+function AddUser (name, pin){
   this.name = name;
-  this.password = password;
+  this.pin = pin;
   users.push(this);
 }
 
-new AddUser 
+function AddChore (name, startDate, frequency){
+  this.name = name;
+  this.startDate = startDate;
+  // startDate defined as days from current date.
+  this.frequency = frequency;
+  // frequency defined as every X day from start day ex: startDate + 1day = scheduled day / chore occurs every 1 day after start date.
+  chores.push(this);
+}
+
+new AddUser('Klace', 666);
+new AddChore ('Sweep', 0, 1);
+
+// console.log(new Date());
