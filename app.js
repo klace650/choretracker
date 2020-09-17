0e176af1588e4796c3365579397078bdd63a5cb7
Klace Koch <koch.klace@gmail.com>
2020-09-17 15:18:41 -0700

    testing

--- app.js
+++ app.js
@@ -19,9 +19,10 @@ function AddUser (name, pin){
 }
 //
 // Adds chores details into global user array
-function AddChore (name, startDate, frequency){
+function AddChore (name, frequency){
+// function AddChore (name, startDate, frequency){  
   this.name = name;
-  this.startDate = startDate;
+  // this.startDate = startDate;
   // startDate defined as days from current date.
   this.frequency = frequency;
   // frequency defined as every X day from start day ex: startDate + 1day = scheduled day / chore occurs every 1 day after start date.
@@ -65,13 +66,19 @@ console.log(chores);
 // EVENT HANDLERS
 // a) may need to use: var name = var startDate = var frequency
 
+
 // handles when user clicks submit on newChore form
 function handleSubmit(event){
   event.preventDefault();
-  var name = event.target.name.value;
-  var startDate = event.target.startDate.value;
-  var frequency = event.target.frequency.value;
+  var name = (event.target.name.value);
+  // var startDate = event.target.startDate.value;
+  var frequency = parseInt(event.target.frequency.value);
+
+  var newChore = new AddChore(name,frequency);
+  // var newChore = new AddChore(name,startDate,frequency);
+  console.log(newChore);
 }
+
 // ...........................................................
 // EVENT LISTENERS
 // 
