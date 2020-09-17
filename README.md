85f246e2fde348f7dc50b51f3cf9d72694dda23d
Klace Koch <koch.klace@gmail.com>
2020-09-17 12:11:27 -0700

    Made constructor for new user

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # choretracker
 my attempt at just trying to do what i had in mind when i pitched the app
+
+## Save These Ideas
+find out the recommended cleaning interval for 
---
+++ app.js
@@ -0,0 +1,20 @@
+// function logSubmit(event) {
+//   log.textContent = `Form Submitted! Time stamp: ${event.timeStamp}`;
+//   event.preventDefault();
+//   console.log(input1)
+// }
+
+// var form = document.getElementById('form');
+// console.log(form)
+
+// form.addEventListener('submit', logSubmit);
+
+var users = []
+
+function AddUser (name){
+  this.name = name;
+  this.password = password;
+  users.push(this);
+}
+
+new AddUser 
