041b6773706b698a5e724653fded2ad202430bd2
Klace Koch <koch.klace@gmail.com>
2020-10-07 22:43:53 -0700

    Starting to work on assigning and building chore list per user

--- app.js
+++ app.js
@@ -1,59 +1,80 @@
 'use strict'
 // ...........................................................
 // GLOBALS
-var users = [];
-var chores = [];
-var currentDate = new Date();
+let userList = [];
+let choreList = [];
+let date = new Date();
+let day = date.getDay();
 
-var form = document.getElementById('newChore');
+// 0 SUNDAY
+// 1 MONDAY
+// 2 TUESDAY
+// 3 WEDNESDAY
+// 4 THURSDAY
+// 5 FRIDAY
+// 6 SATURDAY
 
-// ...........................................................
-// CONSTRUCTOR FUNCTIONS
-// 
-// Adds users details into global user array
-function AddUser (name, pin){
-  this.name = name;
-  this.pin = pin;
-  users.push(this);
-}
-// Adds chores details into global user array
-function AddChore (name, startDate, frequency){  
-  this.name = name;
-  // name of the chore
-  this.startDate = startDate;
-  // startDate defined as days from current date.
-  this.frequency = frequency;
-  // frequency defined as every X day from start day ex: startDate + 1day = scheduled day / chore occurs every 1 day after start date.
-  chores.push(this);
-}
-// ...........................................................
-// RENDER
-//
-
-
-
-// ...........................................................
-// FORMS
-
-
-
-
-// ...........................................................
 // USERS
-new AddUser('Klace', 666);
-new AddUser('Joelle', 555);
+// 
+let klace = new User('Klace');
+let joelle = new User('Joelle');
 
 // ...........................................................
 // CHORES
-new AddChore ('Sweep', 0, 1);
-new AddChore ('Dishes', 0, 1);
+
+let sweep = new Chore('Sweep', 0, 30, 0);
+let dishes = new Chore('Dishes', 0, 30, 0);
+let vacuum = new Chore('Vacuum', 5, 20, 0);
+let unloadDish = new Chore('Unload Dishwasher', 1, 15, 0);
+let loadDish = new Chore('Load Dishwasher', 1, 15, 0);
+let tidyKitchen = new Chore('Tidy Kitchen', 0, 20, 0);
+let laundry = new Chore ('Laundry', 6, 90, 0);
+let cleanKitchen = new Chore('Clean Kitchen', 4, 30, 0);
+let cleanFridge = new Chore('Clean Out Fridge', 7, 20, 0);
+let takeCompost = new Chore('Take out Compost', 7, 10, 0);
+let takeTrash = new Chore('Take out Trash', 7, 20, 0);
+let curbCompost = new Chore ('Put out Compost', 7, 10, 0);
+let curbRecycle = new Chore ('Put out Recycling', 14, 10, 0);
 
 // ...........................................................
-// TEST LOGS
-console.log(new Date());
-console.log(users);
-console.log(chores);
+// CONSTRUCTOR FUNCTIONS
+function User(name){
+  this.name = name;
+  userList.push(this);
+}
+// 
+function Chore(name, repeats, duration, starts){
+  this.name = name;
+  this.repeats = repeats;
+  this.duration = duration; 
+  this.starts = starts;
+  choreList.push(this);
+};
 // ...........................................................
+// 
+function assignChores(){
+  
+  let assignedUser = /* NEEDS TO ITERATE */
+  let assignChore = choreList[0].name;
+  
+  let userChoreList = 
+  console.log(assignChore , assignedUser);
+  // 
+}
+assignChores();
+
+// ...........................................................
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+// TEST LOGS
+console.log()
+// console.log(choreList[5]);
+// Displays individual chore details at index
+// console.log(day);
+// Displays number of current date
+
 // ...........................................................
 // ...........................................................
 // ...........................................................
@@ -65,25 +86,25 @@ console.log(chores);
 // EVENT HANDLERS
 //
 // function newChoreSubmit(event){
-//   // handles when user clicks submit on newChore form
-//   event.preventDefault();
-//   var name = (event.target.name.value);
-//   var frequency = parseInt(event.target.frequency.value);
-//   var startDate = event.target.startDate.value;
-//   var newChore = new AddChore(name,startDate,frequency);
-//   console.log(newChore);
+  //   // handles when user clicks submit on newChore form
+  //   event.preventDefault();
+  //   var name = (event.target.name.value);
+  //   var frequency = parseInt(event.target.frequency.value);
+  //   var startDate = event.target.startDate.value;
+  //   var newChore = new AddChore(name,startDate,frequency);
+  //   console.log(newChore);
   
-// }
-function handleSubmit(event){
-  event.preventDefault
-  var name = event.target.name.value;
-  var pin = parseInt(event.target.pin.value);
-  var newUser = new AddUser(name,pin);
-  console.log(newUser);
-}
-// ...........................................................
-// EVENT LISTENERS
-// 
-// form.addEventListener('submit', newChoreSubmit);
-form.addEventListener('submit', handleSubmit);
-// ...........................................................
+  // }
+  // function handleSubmit(event){
+    //   event.preventDefault
+    //   var name = event.target.name.value;
+    //   var pin = parseInt(event.target.pin.value);
+    //   var newUser = new AddUser(name,pin);
+    //   console.log(newUser);
+    // }
+    // ...........................................................
+    // EVENT LISTENERS
+    // 
+    // form.addEventListener('submit', newChoreSubmit);
+    // form.addEventListener('submit', handleSubmit);
+    // ...........................................................
